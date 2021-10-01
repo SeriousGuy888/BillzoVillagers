609d913604e098a1f66a6bb5aafc9c850de9aaab
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-10-01 10:14:45 -0400

    custom villager last names based on breeding parents

--- src/me/billzo/billzovillagers/Listeners.java
+++ src/me/billzo/billzovillagers/Listeners.java
@@ -6,9 +6,12 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Random;
 
 public class Listeners implements Listener {
   private final JavaPlugin plugin;
@@ -44,5 +47,35 @@ public class Listeners implements Listener {
         player.getInventory().setItemInMainHand(newHeldItem);
       }
     }
+  }
+
+  @EventHandler
+  public void onEntityBreed(EntityBreedEvent event) {
+    LivingEntity child = event.getEntity();
+    LivingEntity mother = event.getMother();
+    LivingEntity father = event.getFather();
+
+    if(!(child instanceof Villager))
+      return;
+    if(mother.getCustomName() == null || father.getCustomName() == null)
+      return;
+
+    String[] firstNames = plugin.getConfig().getStringList("names.first").toArray(new String[0]);
+    if(firstNames.length < 1)
+      return;
+
+    String childFirstName = firstNames[new Random().nextInt(firstNames.length)];
+
+    String motherLastName = mother.getCustomName().substring(mother.getCustomName().lastIndexOf(" ") + 1);
+    String fatherLastName = father.getCustomName().substring(father.getCustomName().lastIndexOf(" ") + 1);
+    String childLastName;
+
+    if(motherLastName.equals(fatherLastName)) {
+      childLastName = String.format("%s-%s", motherLastName, motherLastName);
+    } else {
+      childLastName = new Random().nextInt(2) == 1 ? motherLastName : fatherLastName;
+    }
+
+    child.setCustomName(String.format("%s %s", childFirstName, childLastName));
   }
 }
