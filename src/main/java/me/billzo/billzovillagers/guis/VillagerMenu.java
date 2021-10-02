0915b1ca627b5e5ba47417b990f996a110187884
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-10-02 01:35:59 -0400

    background in villager menu i finally managed to get that library working after all day

--- src/main/java/me/billzo/billzovillagers/guis/VillagerMenu.java
+++ src/main/java/me/billzo/billzovillagers/guis/VillagerMenu.java
@@ -1,6 +1,9 @@
 package me.billzo.billzovillagers.guis;
 
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.pane.OutlinePane;
+import com.github.stefvanschie.inventoryframework.pane.Pane;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -9,6 +12,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class VillagerMenu implements Listener {
   //  private Inventory gui;
@@ -29,11 +33,25 @@ public class VillagerMenu implements Listener {
         guiTitle = "Villager Menu";
 
 
-      ChestGui gui = new ChestGui(1, guiTitle);
+      ChestGui gui = new ChestGui(3, guiTitle);
       gui.setOnGlobalClick(evt -> evt.setCancelled(true));
       gui.setOnGlobalDrag(evt -> evt.setCancelled(true));
 
+      OutlinePane background = new OutlinePane(0, 0, 9, 3, Pane.Priority.LOWEST);
+      background.addItem(new GuiItem(createItem(Material.GRAY_STAINED_GLASS_PANE, 1, " ")));
+      background.setRepeat(true);
+      gui.addPane(background);
+
       gui.show(player);
     }
+  }
+
+  private ItemStack createItem(Material material, int count, String name) {
+    ItemStack item = new ItemStack(material);
+    item.setAmount(count);
+    ItemMeta meta = item.getItemMeta();
+    meta.setDisplayName(name);
+    item.setItemMeta(meta);
+    return item;
   }
 }
